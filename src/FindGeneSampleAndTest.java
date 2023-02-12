5af92242c5404c918b064a9b9f5516049902e6cf
unknown <georgeankumensah@gmail.com>
2023-02-12 07:38:15 +0000

    added gene checker

--- src/FindGeneSampleAndTest.java
+++ src/FindGeneSampleAndTest.java
@@ -7,7 +7,13 @@ public class FindGeneSampleAndTest {
             int endIndex = dna.indexOf("TAA");
             int endOfGene = endIndex + 3;
             String result = dna.substring(startIndex,endOfGene);
+            if (result.length() % 3 == 0) {
+                System.out.println("Gene is an original");
+            } else {
+                System.out.println("Gene is a fake");
+            }
             System.out.println(result);
+
         } else {
             System.out.println("Gene not found");
         }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         FindGeneSampleAndTest.findGeneSampleAndTest("ATGCGATACGCTTGAATTAACGCTTGACTAATCGCTGCTAGTAACGCTGCTAG");
-        System.out.println("Hello world!");
     }
 }
